bc1416b3b33a3bb1f322d766946016bc529e37ad
sidraya <131861300+sidraya@users.noreply.github.com>
2024-09-11 23:00:34 +0530

    code change

---
+++ abstraction_demo.py
@@ -0,0 +1,20 @@
+from abstract_class import Vehicale
+class Bike(Vehicale):
+    def __int__(self,n,color):
+        super().__int__(n)
+        self.color=color
+    def start(self):
+        print("Start with kick")
+class Scooty(Vehicale):
+    def __int__(self,n):
+        self.no_of_tyres=n
+    def start(self):
+        print("self start")
+class Car(Vehicale):
+    def __int__(self,n,x):
+        super().__int__(n)
+        self.no_of_gears=6
+    def start(self):
+        print("start with key")
+
+v=Vehicale
